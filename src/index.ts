ab8d48a4927b99b79f106c2aa16d5410beea3043
dz1to4ka <117407093+illiaBabak@users.noreply.github.com>
2025-06-02 11:00:56 +0200

    Added cors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import express from "express";
 import usersRouter from "./routes/users";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
 app.use("/users", usersRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
