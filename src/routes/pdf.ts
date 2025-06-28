0cacfbd68f894907fb61d0b2ef936516da44a418
dz1to4ka <117407093+illiaBabak@users.noreply.github.com>
2025-06-28 14:38:38 +0200

    Added routes /get-documents and /get-document

--- src/routes/pdf.ts
+++ src/routes/pdf.ts
@@ -9,8 +9,11 @@ import {
   getLatestVersionOfChartType,
   updateChart,
   uploadPdf,
+  getChart,
+  getCharts,
 } from "../services/supabaseServices";
 import { pdfStreamToBuffer } from "../utils/pdfStreamToBuffer";
+import { isString } from "../utils/guards";
 
 const router = express.Router();
 
@@ -59,6 +62,35 @@ router.post("/generate-document", async (req: Request, res: Response) => {
     res
       .status(500)
       .json({ error: "Internal server error on generate document" });
+  }
+});
+
+router.get("/get-documents", async (req: Request, res: Response) => {
+  try {
+    const charts = await getCharts();
+
+    res.json(charts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error on get documents" });
+  }
+});
+
+router.get("/get-document", async (req: Request, res: Response) => {
+  try {
+    const { key } = req.query;
+
+    if (!isString(key)) {
+      res.status(400).json({ error: "Key is required" });
+      return;
+    }
+
+    const chart = await getChart(key);
+
+    res.json(chart);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error on get document" });
   }
 });
 
